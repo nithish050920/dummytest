d55e06623cec73d79deb5549b6620d7da15cfb02
nithish050920 <nithishkumarrnk0@gmail.com>
2022-08-06 15:16:15 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    document.getElementById("demo").style.display="block";
+}
+function printFunc(){
+    window.print();
+}
+{
+let a,b,c;
+a=10;
+b=20;
+c=a+b;
+document.getElementById('add').innerText=c;
+}
+{
+    let a={name:"benz",color:"white",price:5000000};
+    document.getElementById('obj').innerHTML='the price of '+a.name+' '+'is'+' '+a.price;
+}
